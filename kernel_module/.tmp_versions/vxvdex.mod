0b427c87a8f9741a6160e617a3124ddfd89bb116
Renzo Davoli <renzo@cs.unibo.it>
2016-10-08 11:27:16 +0200

    close poisoning fix+repo cleanup

--- kernel_module/.tmp_versions/vxvdex.mod
+++
@@ -1,2 +0,0 @@
-/home/renzo/tests/libvdeplug4/vxvdex/kernel_module/vxvdex.ko
-/home/renzo/tests/libvdeplug4/vxvdex/kernel_module/af_vxvdex.o
--- kernel_module/.vxvdex.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/renzo/tests/libvdeplug4/vxvdex/kernel_module/vxvdex.o := ld -m elf_x86_64   -r -o /home/renzo/tests/libvdeplug4/vxvdex/kernel_module/vxvdex.o /home/renzo/tests/libvdeplug4/vxvdex/kernel_module/af_vxvdex.o 
